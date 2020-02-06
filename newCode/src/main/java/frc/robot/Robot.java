cad9c334ee0a41aba84be44b7d23822df5ca4d14
Pr3ttyneatguy <59533152+Pr3ttyneatguy@users.noreply.github.com>
2020-02-05 19:03:29 -0500

    Encapsulated Motors

--- newCode/src/main/java/frc/robot/Robot.java
+++ newCode/src/main/java/frc/robot/Robot.java
@@ -34,32 +34,125 @@ public class Robot extends TimedRobot {
   Limelight limelight=new Limelight();
 
 
-  CANSparkMax br = new CANSparkMax(12, MotorType.kBrushless);
-  CANSparkMax fr = new CANSparkMax(13, MotorType.kBrushless);
-  CANSparkMax fl = new CANSparkMax(11, MotorType.kBrushless);
-  CANSparkMax bl = new CANSparkMax(10, MotorType.kBrushless);
-  public CANSparkMax centerIntakeFront=new CANSparkMax(17, MotorType.kBrushless);
-  public CANSparkMax centerIntakeBack=new CANSparkMax(16, MotorType.kBrushless);
-  public CANSparkMax verticalLoader=new CANSparkMax(18, MotorType.kBrushless);
-  public CANSparkMax outerIntakeBack = new CANSparkMax(14,MotorType.kBrushless);
-  public CANSparkMax outerIntakeFront = new CANSparkMax(15,MotorType.kBrushless);
-  public CANSparkMax meteringWheel = new CANSparkMax(19,MotorType.kBrushless);
-  public CANSparkMax leftShooterWheel = new CANSparkMax(20,MotorType.kBrushless);
-  public CANSparkMax rightShooterWheel = new CANSparkMax(21,MotorType.kBrushless);
-  public CANSparkMax turret = new CANSparkMax(22,MotorType.kBrushless);
-  public CANSparkMax shooterElevator = new CANSparkMax(23,MotorType.kBrushless);
-  public CANSparkMax climb1 = new CANSparkMax(24,MotorType.kBrushless);
-  public CANSparkMax climb2 = new CANSparkMax(25,MotorType.kBrushless);
+  private CANSparkMax br = new CANSparkMax(12, MotorType.kBrushless);
+  private CANSparkMax fr = new CANSparkMax(13, MotorType.kBrushless);
+  private CANSparkMax fl = new CANSparkMax(11, MotorType.kBrushless);
+  private CANSparkMax bl = new CANSparkMax(10, MotorType.kBrushless);
+  private CANSparkMax centerIntakeFront=new CANSparkMax(17, MotorType.kBrushless);
+  private CANSparkMax centerIntakeBack=new CANSparkMax(16, MotorType.kBrushless);
+  private CANSparkMax verticalLoader=new CANSparkMax(18, MotorType.kBrushless);
+  private CANSparkMax outerIntakeBack = new CANSparkMax(14,MotorType.kBrushless);
+  private CANSparkMax outerIntakeFront = new CANSparkMax(15,MotorType.kBrushless);
+  private CANSparkMax meteringWheel = new CANSparkMax(19,MotorType.kBrushless);
+  private CANSparkMax leftShooterWheel = new CANSparkMax(20,MotorType.kBrushless);
+  private CANSparkMax rightShooterWheel = new CANSparkMax(21,MotorType.kBrushless);
+  private CANSparkMax turret = new CANSparkMax(22,MotorType.kBrushless);
+  private CANSparkMax shooterElevator = new CANSparkMax(23,MotorType.kBrushless);
+  private CANSparkMax climb1 = new CANSparkMax(24,MotorType.kBrushless);
+  private CANSparkMax climb2 = new CANSparkMax(25,MotorType.kBrushless);
 
+  public void setCenterIntakeFront(double speed) {
+    centerIntakeFront.set(speed);
+  }
+
+  public double getCenterIntakeFront() {
+    return centerIntakeFront.get();
+  }
   
+  public void setCenterIntakeBack(double speed) {
+    centerIntakeBack.set(speed);
+  }
 
+  public double getCenterIntakeBack() {
+    return centerIntakeBack.get();
+  }
+
+  public void setVerticalLoader(double speed) {
+    verticalLoader.set(speed);
+  }
+
+  public double getVerticalLoader() {
+    return verticalLoader.get();
+  }
+
+  public void setOuterIntakeBack(double speed) {
+    outerIntakeBack.set(speed);
+  }
+
+  public double getOuterIntakeBack() {
+    return outerIntakeBack.get();
+  }
+
+  public void setOuterIntakeFront(double speed) {
+    outerIntakeFront.set(speed);
+  }
+
+  public double getOuterIntakeFront() {
+    return outerIntakeFront.get();
+  }
+
+  public void setMeteringWheel(double speed) {
+    meteringWheel.set(speed);
+  }
+
+  public double getMeteringWheel() {
+    return meteringWheel.get();
+  }
+
+  public void setLeftShooterWheel(double speed) {
+    leftShooterWheel.set(speed);
+  }
+
+  public double getLeftShooterWheel() {
+    return leftShooterWheel.get();
+  }
+
+  public void setRightShooterWheel(double speed) {
+    rightShooterWheel.set(speed);
+  }
+
+  public double getRightShooterWheel() {
+    return rightShooterWheel.get();
+  }
+
+  public void setTurret(double speed) {
+    turret.set(speed);
+  }
+
+  public double getTurret() {
+    return turret.get();
+  }
+
+  public void setShooterElevator(double speed) {
+    shooterElevator.set(speed);
+  }
+
+  public double getShooterElevator() {
+    return shooterElevator.get();
+  }
+
+  public void setClimb1(double speed) {
+    climb1.set(speed);
+  }
+
+  public double getClimb1() {
+    return climb1.get();
+  }
+
+  public void setClimb2(double speed) {
+    climb2.set(speed);
+  }
+
+  public double getClimb2() {
+    return climb2.get();
+  }
 
   DifferentialDrive driveControl;
   Joystick leftJoystick = new Joystick(0);
   Joystick rightJoystick = new Joystick(1);
   Joystick tractorJoystick = new Joystick(2);
   JLSBAdapter leftJoystickAdapter = new JLSBAdapter(leftJoystick, this);
-  JLSBAdapter rightJoystickAdapter = new JLSBAdapter(rightJoystick, this)
+  JLSBAdapter rightJoystickAdapter = new JLSBAdapter(rightJoystick, this);
   TSBAdapter tractorAdapter = new TSBAdapter(tractorJoystick, this);
 
   public static EventHandler eHandler=new EventHandler();
