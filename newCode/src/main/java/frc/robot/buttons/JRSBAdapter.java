f4df438673b13b56c01c18dfcd826b0f765651bb
Pr3ttyneatguy <59533152+Pr3ttyneatguy@users.noreply.github.com>
2020-02-07 19:25:41 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- newCode/src/main/java/frc/robot/buttons/JRSBAdapter.java
+++ newCode/src/main/java/frc/robot/buttons/JRSBAdapter.java
@@ -2,6 +2,7 @@ package frc.robot.buttons;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Robot;
 
 /**
@@ -9,6 +10,18 @@ import frc.robot.Robot;
  */
 public class JRSBAdapter extends ButtonHandler{
     private Robot robot;
+
+    boolean isArcade = true;
+    boolean isTank= false;
+
+    public boolean isArcade(){
+        return isArcade;
+    }
+
+    public boolean isTank(){
+        return isTank;
+    }
+
     public JRSBAdapter(Joystick joystick, Robot robot){
         super(joystick,12);
         this.robot=robot;
@@ -17,24 +30,34 @@ public class JRSBAdapter extends ButtonHandler{
     public void buttonPressed(int no){
         switch (no){
             case 1:
-                robot.leftShooterWheel.set(-.5);
-                robot.rightShooterWheel.set(-.5);
+                robot.leftShooterWheel().set(-.5);
+                robot.rightShooterWheel().set(-.5);
             break;
             case 2:
-                robot.outerIntakeFront.set(-.5);
-                robot.outerIntakeBack.set(.5);
+                robot.outerIntakeFront().set(-.5);
+                robot.outerIntakeBack().set(.5);
             break;
         }  
     }
     public void buttonReleased(int no){
         switch (no){
             case 1:
-                robot.leftShooterWheel.set(0);
-                robot.rightShooterWheel.set(0);
+                robot.leftShooterWheel().set(0);
+                robot.rightShooterWheel().set(0);
             break;
             case 2:
-                robot.outerIntakeFront.set(0);
-                robot.outerIntakeBack.set(0);
+                robot.outerIntakeFront().set(0);
+                robot.outerIntakeBack().set(0);
+            break;
+            case 11:
+                
+                if (isArcade){
+                    isTank=true;
+                    isArcade=false;
+                }else{
+                    isTank=false;
+                    isArcade=true;                    
+                }
             break;
         }
     }
