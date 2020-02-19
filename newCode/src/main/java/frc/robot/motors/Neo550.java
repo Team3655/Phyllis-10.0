53de9ea3ca4afba7d616c8221972cad4e6b2f4eb
BrendanMumby <47483188+BrendanMumby@users.noreply.github.com>
2020-02-19 18:47:21 -0500

    Revert "Neo550 no longer implements Runnable"
    This reverts commit 2421aa3524052b119c8ce39841e60037c15d7be3.

--- newCode/src/main/java/frc/robot/motors/Neo550.java
+++ newCode/src/main/java/frc/robot/motors/Neo550.java
@@ -2,17 +2,16 @@ package frc.robot.motors;
 
 import com.revrobotics.CANSparkMax;
 
-public class Neo550 extends CANSparkMax /*implements Runnable*/{
+public class Neo550 extends CANSparkMax implements Runnable{
 
     public Neo550(int deviceID) {
         super(deviceID, MotorType.kBrushless);
         setSmartCurrentLimit(40);//fourty amp limit
     }
 
-    /*
     @Override
     public void run(){
         //temp limit goes here
-    }*/
+    }
 
 }
