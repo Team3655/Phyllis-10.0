e7ffa1dacff4f1814c4f50b99643fd698f57e599
Pr3ttyneatguy <59533152+Pr3ttyneatguy@users.noreply.github.com>
2020-03-04 18:21:30 -0500

    Fixed differential drive
    set it to null

--- newCode/src/main/java/frc/robot/Robot.java
+++ newCode/src/main/java/frc/robot/Robot.java
@@ -125,7 +125,7 @@ public class Robot extends TimedRobot {
     
     //centerIntakeFront.clearFaults();
     eHandler.start();
-    driveControl = new DifferentialDrive(fl, fr);// new DifferentialDrive(fl, fr);
+    // new DifferentialDrive(fl, fr);
     //driveControl.
     br.follow(fr);
     bl.follow(fl);
@@ -192,6 +192,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     //limelight.enable();
+    driveControl=null;
     
     try{
       tractorAdapter.setMode(TSBAdapter.Mode.Tune);
@@ -211,7 +212,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     //limelight.enable();
-    
+    driveControl = new DifferentialDrive(fl, fr);
     try{
       tractorAdapter.setMode(TSBAdapter.Mode.RobotResponse);
     } catch (Exception e){
